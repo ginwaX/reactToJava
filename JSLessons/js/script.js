e3eae957ae5d382825a66cae95efc5f9514e0763
pepega <pepega@pepegas-MacBook-Pro.local>
2025-10-06 13:58:41 +0300

    lesson 9 stuff

--- JSLessons/js/script.js
+++ JSLessons/js/script.js
@@ -1,1 +1,10 @@
-console.log("xffing");
+let num = 5;
+const leftBorder = 10;
+
+const obj = {
+    a: 50
+};
+
+obj.a = 67;
+
+var pepega = 'omega'; // can change but bruh, it exists before declaration
