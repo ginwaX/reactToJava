00d3822d9f8d36fd3eeccc4b474d2c846b9d5962
pepega <pepega@pepegas-MacBook-Pro.local>
2025-10-06 14:41:42 +0300

    lesson 13 stuff

---
+++ JSLessons/js/scriptNewLesons.js
@@ -0,0 +1,12 @@
+//alert('manMode');
+
+//const result = confirm('what is your parse on DSR?');
+
+//const answer = prompt('is perfect alexander perfect?', 'yes ofc');
+//console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('are we tired dragon king thordan?', '');
+answers[1] = prompt('are we tired perfect alexander?', '');
+answers[2] = prompt('are we tired omage my beloved?', '');
