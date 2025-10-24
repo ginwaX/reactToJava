56ea26f3602f531225a68b0708b212b24e9ceb52
pepega <pepega@pepegas-MacBook-Pro.local>
2025-10-24 12:31:46 +0300

    added code for the latest modal and class things

--- JSLessons/js/lesson4.2/src/js/main.js
+++ JSLessons/js/lesson4.2/src/js/main.js
@@ -96,9 +96,91 @@ window.addEventListener('DOMContentLoaded', () => {
             btn.addEventListener('click', function() {
                 modal.classList.add('show');
                 modal.classList.remove('hide');
-                // Либо вариант с toggle - но тогда назначить класс в верстке
-                document.body.style.overflow = 'hidden';
             });
         });
 
+        function openModal() {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+        }
+
+        function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+        }
+        
+        modalCloseBtn.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                closeModal();
+            }
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "Escape" && modal.classList.contains('show')) { 
+                closeModal();
+            }
+        });
+
+        class MenuCard {
+            constructor(src, alt, title, descr, price, parentSelector) {
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.parent = document.querySelector(parentSelector);
+                this.transfer = 27;
+                this.changeToUAH(); 
+            }
+
+            changeToUAH() {
+                this.price = this.price * this.transfer; 
+            }
+
+            render() {
+                const element = document.createElement('div');
+                element.innerHTML = `
+                    <div class="menu__item">
+                        <img src=${this.src} alt=${this.alt}>
+                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                        <div class="menu__item-descr">${this.descr}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                            <div class="menu__item-cost">Цена:</div>
+                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                        </div>
+                    </div>
+                `;
+                this.parent.append(element);
+            }
+        }
+
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            9,
+            ".menu .container"
+        ).render();
+
+        new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            14,
+            ".menu .container"
+        ).render();
+
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            21,
+            ".menu .container"
+        ).render();
 });
